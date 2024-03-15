a77ff9fba64d7e8f30f856f0e57406f80928ee99
Furkan Baldır <furkanbaldir13@gmail.com>
2024-03-15 16:36:55 +0300

    Fix build error.

--- Assets/CameraShakeManager/Runtime/Scripts/Editor/CameraShakeSourceEditor.cs
+++ Assets/CameraShakeManager/Runtime/Scripts/Editor/CameraShakeSourceEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 
 namespace CameraShakeManager
@@ -17,3 +18,4 @@ namespace CameraShakeManager
         }
     }
 }
+#endif
